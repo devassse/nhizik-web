cf750333f779d9ac89de9fab383add79ebb2d87a
João Devson Mucavel <joao.mucavel@ideiasdinamicas.com>
2022-08-29 16:12:25 +0200

    Setting up home page

---
+++ src/components/NavBar.vue
@@ -0,0 +1,91 @@
+<template>
+  <header id="header" class="page-header fixed">
+      <div class="navbar navbar-expand-lg">
+        <a href="#" class="navbar-brand text-white"
+          ><svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+          >
+            <circle
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="1"
+              fill="#000"
+            ></circle>
+            <circle
+              cx="12"
+              cy="12"
+              r="2"
+              stroke="currentColor"
+              stroke-width="1"
+            ></circle>
+            <g
+              class="loading-spin"
+              stroke="currentColor"
+              stroke-width="1"
+              style="transform-origin: 12px 12px"
+            >
+              <path
+                d="M16.24 7.76a6 6 0 0 1 0 8.49m-8.48-.01a6 6 0 0 1 0-8.49m11.31-2.82a10 10 0 0 1 0 14.14m-14.14 0a10 10 0 0 1 0-14.14"
+              ></path>
+            </g>
+          </svg>
+          <span class="hidden-folded d-inline l-s-n-1x">Nuzik</span></a
+        >
+        <div
+          class="collapse navbar-collapse order-2 order-lg-1"
+          id="navbarCollapse"
+        >
+          <ul class="navbar-nav" data-nav>
+            <li class="nav-item">
+              <a href="#" class="nav-link"
+                ><span class="nav-text">Artistas</span></a
+              >
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link no-ajax"
+                ><span class="nav-text">Músicas</span></a
+              >
+            </li>
+          </ul>
+        </div>
+        <ul class="nav navbar-menu order-1 order-lg-2">
+          <li class="nav-item">
+            <a class="nav-link no-ajax" href="html/signin.html">Login</a>
+          </li>
+          <li class="d-flex align-items-center">
+            <a
+              class="btn btn-rounded btn-sm btn-primary no-ajax"
+              href="html/signup.html"
+              >Registar</a
+            >
+          </li>
+          <li class="nav-item d-lg-none">
+            <a
+              href="#"
+              class="nav-link px-2"
+              data-toggle="collapse"
+              data-toggle-class
+              data-target="#navbarCollapse"
+              ><i data-feather="menu"></i
+            ></a>
+          </li>
+        </ul>
+      </div>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
